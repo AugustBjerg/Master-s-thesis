9c5cc2c2bc303348cb23dcb37e5f5b7c10a918e3
August Bjerg-Heise <aubj21ab@student.cbs.dk>
2025-11-04 10:33:37 +0800

    Corrected unit for revolutions

--- code/data/cleaning-scripts/metadata.py
+++ code/data/cleaning-scripts/metadata.py
@@ -7,9 +7,22 @@ input_path = os.path.join(parent_dir, 'metadata', 'Metrics registration.xlsx')
 output_path = os.path.join(parent_dir, 'metadata', 'Metrics registration.csv')
 sheet_name = 'Sheet1'
 
+def correct_vessel_propeller_shaft_revolutions_unit(df):
+    # set the value for "unit" of Vessel Propeller Shaft Revolutions to "revs"
+    df.loc[df['quantity_name'] == 'Vessel Propeller Shaft Revolutions', 'unit'] = 'revs'
+
+    # add a parenthesis (cumulative) to the quantity_name for vessel propeller shaft revolutions
+    df.loc[df['quantity_name'] == 'Vessel Propeller Shaft Revolutions', 'quantity_name'] = 'Vessel Propeller Shaft Revolutions (cumulative)'
+
+    return df
+
 def convert_xlsx_to_csv(input_path, output_path):
     # Read the Excel file from the specified sheet
     df = pd.read_excel(input_path, sheet_name=sheet_name)
+
+    # Correct the unit for Vessel Propeller Shaft Revolutions
+    df = correct_vessel_propeller_shaft_revolutions_unit(df)
+    
     # Convert to CSV
     df.to_csv(output_path, index=False)
 
