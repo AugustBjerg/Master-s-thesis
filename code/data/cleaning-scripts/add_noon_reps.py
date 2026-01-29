ea03cde05707c5f4e02c2f059aca94102ac76d8b
August Bjerg-Heise <aubj21ab@student.cbs.dk>
2026-01-29 11:37:17 +0100

    separate file for adding noon report data

---
+++ code/data/cleaning-scripts/add_noon_reps.py
@@ -0,0 +1,5 @@
+import os
+import glob
+import pandas as pd
+import numpy as np
+from loguru import logger
---
+++ code/data/cleaning-scripts/config.py
@@ -0,0 +1,4 @@
+"""Configuration constants for data cleaning scripts."""
+
+# Expected number of sensor observations in the complete dataset
+EXPECTED_SENSOR_OBSERVATIONS = 41079968
