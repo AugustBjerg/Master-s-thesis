b5f98a15df4a29d5781a3137f42b93f10a820f4c
AugustBjerg <august.bjergh@gmail.com>
2026-02-04 15:25:56 +0100

    WIP - laying out approach for aggregation

---
+++ code/data/cleaning-scripts/aggregate.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import os
+from datetime import datetime
+
+# Determine the granularity
+
+# set aggregation functions for each statistic
+
+# Handle NaN values
+    # What only some values are NaN?
+    # What if all values are NaN?
+
+# What happens when there are not observations in that time period?
+
