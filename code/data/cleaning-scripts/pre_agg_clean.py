0e95a264cbcfe90ec9db05d1c38b1891273b3859
AugustBjerg <august.bjergh@gmail.com>
2026-02-04 14:56:13 +0100

    WIP - pre-agg cleaning

--- code/data/cleaning-scripts/pre_agg_clean.py
+++ code/data/cleaning-scripts/pre_agg_clean.py
@@ -1,1 +1,3 @@
 # TODO: clean value column on noon report data from scale or unit-related contamination
+# TODO: Replace negative speed values with NaN
+# TODO: Replace sentinel values with (?) - i want to retain the information that the sensor was failing, so maybe NaN is not the best option
