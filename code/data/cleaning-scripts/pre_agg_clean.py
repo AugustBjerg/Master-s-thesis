41c475b7c130fa990201ae6f61829f2dfe993659
AugustBjerg <august.bjergh@gmail.com>
2026-02-11 10:36:52 +0100

    WIP - cleaning todos

--- code/data/cleaning-scripts/pre_agg_clean.py
+++ code/data/cleaning-scripts/pre_agg_clean.py
@@ -3,12 +3,19 @@
 
 # --- Sentinel / dropout values (no valid measurement) ---
 # TODO: Replace sentinel values with NaN, 
+# TODO: replace negative values of main engine rotation with NaN (it cannot be negative)
 
-# --- NaN removal ---
 
-# --- CLEANING / REMOVAL (including outliers) ---
-# TODO: handle outlier now or after aggregation?
+# --- NaN removal where appropriate ---
+
+# --- CLEANING of "wrong" data ---
+# TODO: remove any signs of a ship "in reverse"
+    # 1. Negative speed values (remove rows)
+    # 2. negative propeller shaft rotational speed (remove rows)
+
 # TODO: deal with observations with incongruent main engine fuel load and shaft power
+
+# --- Outlier removal ---
 
 # --- NaN imputation ---
 # TODO: when imputing, keep a dummy column that flags "imputed" so i retain the information that this was a bad measurement
