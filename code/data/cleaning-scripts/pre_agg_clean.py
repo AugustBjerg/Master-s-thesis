c0378026a47d31896bef532047abb11abd286992
AugustBjerg <august.bjergh@gmail.com>
2026-02-11 10:10:14 +0100

    WIP - sync validation

--- code/data/cleaning-scripts/pre_agg_clean.py
+++ code/data/cleaning-scripts/pre_agg_clean.py
@@ -1,6 +1,12 @@
-# TODO: clean value column on noon report data from scale or unit-related contamination
+# TODO: (optional - if noon report data is included)clean value column on noon report data from scale or unit-related contamination
 # TODO: Replace negative speed values with NaN
-# TODO: Remove outliers now or later?
 
-# TODO: Replace sentinel values with (?) - i want to retain the information that the sensor was failing, so maybe NaN is not the best option
+# --- Sentinel / dropout values (no valid measurement) ---
+# TODO: Replace sentinel values with NaN, but add a dummy column that  - i want to retain the information that the sensor was failing, so maybe NaN is not the best option
+
+# --- NaN removal ---
+
+# --- CLEANING / REMOVAL ---
 # TODO: handle outlier now or after aggregation?
+
+# --- NaN imputation ---
