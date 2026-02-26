8132c0780869e22d556fa92ba7481d2b5a91d3b2
August Bjerg-Heise <aubj21ab@student.cbs.dk>
2026-02-26 15:12:22 +0100

    Added powershell version of data pipeline script

---
+++ run_data_pipeline.ps1
@@ -0,0 +1,24 @@
+echo "data pipeline started"
+
+python code/data/cleaning-scripts/metadata.py
+echo "metadata.py executed"
+
+python code/data/cleaning-scripts/append.py
+echo "append.py executed"
+
+python code/data/cleaning-scripts/add_noon_reps.py
+echo "add_noon_reps.py executed"
+
+python code/data/cleaning-scripts/synchronize.py
+echo "synchronize.py executed"
+
+python code/data/cleaning-scripts/pre_agg_clean.py
+echo "pre_agg_clean.py executed"
+
+python code/data/cleaning-scripts/aggregate.py
+echo "aggregate.py executed"
+
+python code/data/cleaning-scripts/engineer_features.py
+echo "engineer_features.py executed"
+
+echo "data pipeline finished"
