3d55cf7b2720283f10c88ebd46fc35c23e29639b
August Bjerg-Heise <aubj21ab@student.cbs.dk>
2026-03-12 15:45:34 +0100

    WIP - black box models

--- run_entire_pipeline.ps1
+++ run_entire_pipeline.ps1
@@ -33,4 +33,8 @@ echo "gam.py executed"
 python code/modelling/random_forest.py
 echo "random_forest.py executed"
 
+python code/modelling/NN.py
+echo "NN.py executed"
+
+
 echo "data pipeline finished"
--- run_entire_pipeline.sh
+++ run_entire_pipeline.sh
@@ -33,4 +33,7 @@ echo "gam.py executed"
 python code/modelling/random_forest.py
 echo "random_forest.py executed"
 
+python code/modelling/NN.py
+echo "NN.py executed"
+
 echo "data pipeline finished"
